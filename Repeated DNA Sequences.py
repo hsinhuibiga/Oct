b3d45a902f11f86e15fa84f3d4de37c84c456991
hsinhuibiga <hsinhuibiga@gmail.com>
2020-10-18 09:17:43 +0800

    Add files via upload

---
+++ Repeated DNA Sequences.py
@@ -0,0 +1,18 @@
+#Repeated DNA Sequences
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        seen = set()
+        repeated = set()
+        N = len(s)
+        for i in range(N):
+            cur = s[i : i+ 10]
+            if cur in seen:
+                repeated.add(cur)
+            else:
+                seen.add(cur)
+        return list(repeated)
