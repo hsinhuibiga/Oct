5e82c095344be006256803b3ab25c0609cb7c42e
hsinhuibiga <hsinhuibiga@gmail.com>
2020-10-20 23:44:57 +0800

    Add files via upload

---
+++ Factorial Trailing Zeroes.py
@@ -0,0 +1,14 @@
+#Factorial Trailing Zeroes
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        i = 5
+        while n >= i:
+            res += n / i
+            i *= 5
+        return res
