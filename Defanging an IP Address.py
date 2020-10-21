26adef1350f592d449a32dbf28d8573b5efdb063
hsinhuibiga <hsinhuibiga@gmail.com>
2020-10-21 23:19:37 +0800

    Add files via upload

---
+++ Defanging an IP Address.py
@@ -0,0 +1,9 @@
+#Defanging an IP Address
+
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        return address.replace('.','[.]')
